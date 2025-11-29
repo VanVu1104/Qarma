0c8504df1971092880bb2cba03653670d9787738
Võ Tấn Dũng <22dh110638@st.huflit.edu.vn>
2025-11-29 11:09:55 +0700

    supplier overview

--- Qarma/ViewModels/SupplierStatsViewModel.cs
+++ Qarma/ViewModels/SupplierStatsViewModel.cs
@@ -8,13 +8,21 @@ namespace Qarma.ViewModels
 	public class SupplierStatsViewModel
 	{
 		public string SupplierName { get; set; }
+
+		// Các chỉ số lỗi
 		public int Minor { get; set; }
 		public int Major { get; set; }
 		public int Critical { get; set; }
-		public int SumDefects => Minor + Major + Critical; // Tự động tính
+		public int SumDefects => Minor + Major + Critical + GeneralDefects;
+		public int GeneralDefects { get; set; }
+		public int SampleQuantity { get; set; }
+		public int AvailableQuantity { get; set; }
 		public double DefectRate { get; set; }
 		public int Inspections { get; set; }
+		public double RejectionRate { get; set; }
+		public double ReInspectionRate { get; set; }
+		public double LocationRate { get; set; }
+		public double ParetoDefect { get; set; }      // Dùng để vẽ đường % Defect
 		public double ParetoInspection { get; set; }
-		public double ParetoDefect { get; set; }
 	}
 }
